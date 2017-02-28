6e629f5d8a0557219ebd2a197bdb060dff2b37e7
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-27 22:21:13 -0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Erp::Purchase::Engine.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
 		namespace :backend, module: "backend", path: "backend/purchase" do
-			resources :orders do
-				collection do
-					post 'list'
-				end
-			end
+			get 'purchase_orders' => 'purchases#purchase_orders', as: :purchase_orders
+			post 'purchase_orders_listing' => 'purchases#purchase_orders_listing', as: :purchase_orders_listing
+			get 'purchase_order_details' => 'purchases#purchase_order_details', as: :purchase_order_details
 		end
 	end
 end
